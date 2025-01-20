4b91624aeebf04af996a7a316f17a29060f92c30
Igor Diogo <igordiogo1314@gmail.com>
2025-01-20 19:29:50 -0300

    Cadastro e exibição de pratos

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask  import Flask, render_template #importando a biblioteca flask
+from flask  import Flask, render_template, request #importando a biblioteca flask
 from flask_sqlalchemy import SQLAlchemy #importando a biblioteca sqlachemy
 from flask_wtf import FlaskForm #importando a biblioteca flaskForm
-from wtforms import Form, StringField, SubmitField, PasswordField, BooleanField, SubmitField, SelectField, FloatField
+from wtforms import Form, StringField, SubmitField, SubmitField, SelectField, FloatField
 
 app = Flask(__name__)# criando uma instância da classe Flask
 app.config['SECRET_KEY'] = 'DIE'
@@ -9,20 +9,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TM.db'
 
 bancoDados = SQLAlchemy(app)#criando uma instância da classe SQLAlchemy
 
+class RegistrationForm(FlaskForm):
+    nome = StringField('Nome')
+    preco = FloatField('Preco')
+    categoria = SelectField('Categoria',  choices=[('entrada','Entrada'),('principal', 'Principal'), ('sobremesa', 'Sobremesa')])
+    submit = SubmitField('Cadastrar')
 
-class Pratos():
+class Pratos(bancoDados.Model):
     id = bancoDados.Column(bancoDados.Integer, primary_key = True, autoincrement = True)
     nome = bancoDados.Column(bancoDados.String(100), nullable = False)
     preco = bancoDados.Column(bancoDados.Float, nullable = False)
     categoria = bancoDados.Column(bancoDados.String(100), nullable = False)
     
-class CasdastroForm(FlaskForm):
-    nome = StringField('Nome')
-    preco = FloatField('Preco')
-    categoria = SelectField('Categoria')
-    submit = SubmitField('Cadastrar')
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -41,13 +39,22 @@ def cardapio():
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
-        if Form.validade_on_submit():
-            Pratos =Pratos(nome = Form.nome.data, preco = Form.preco.data, categoria = Form.categoria.data) 
-            bancoDados.session.add(Pratos)
-            bancoDados.session.commit()
-            return 'Prato cadastrado com sucesso!'
-        return render_template('cadastro.html', Form = Form)
+    form = RegistrationForm(request.form)
+    if form.validate() and request.method == "POST":
+        pratos = Pratos(
+            nome = form.nome.data, 
+            preco = form.preco.data, 
+            categoria = form.categoria.data) 
+        bancoDados.session.add(pratos)
+        bancoDados.session.commit()
+        return 'Prato cadastrado com sucesso!'
+    
+    return render_template('cadastro.html', form = form)
 
+@app.route('/listarDados')
+def listarDados():
+    dados = Pratos.query.all()
+    return render_template('listarDados.html', dados=dados)
 
 if __name__ == '__main__':
     with app.app_context():
