2450e311728aa562faa219501e6476d2533cdcbd
Igor Diogo <igordiogo1314@gmail.com>
2025-02-03 18:25:44 -0300

    aaaa

--- static/js/comand.js
+++ static/js/comand.js
@@ -1,4 +1,3 @@
-
 menu = document.getElementById('itens');
 
 function clickMenu(){
@@ -6,5 +5,25 @@ function clickMenu(){
         menu.style.display = 'none'
     }else{
         menu.style.display = 'flex'
+    }
+}
+
+function ativaCategoria(categoria){
+    entrada = document.getElementById("mainEntrada");
+    principal = document.getElementById("mainPrincipal");
+    sobremesa = document.getElementById("mainSobremesa");
+
+    if(categoria=="mainEntrada"){
+        entrada.style.display = "grid";
+        principal.style.display = "none";
+        sobremesa.style.display = "none";
+    }else if(categoria=="mainPrincipal"){
+        entrada.style.display = "none";
+        principal.style.display = "grid";
+        sobremesa.style.display = "none";
+    }else if(categoria=="mainSobremesa"){
+        entrada.style.display = "none";
+        principal.style.display = "none";
+        sobremesa.style.display = "grid";
     }
 }
